b95aa91598c177f7fae163a1bd02762dd759946d
Daniel Jackson <daniejackson@deloitte.com>
2023-10-26 18:01:00 -0500

    this is the summary of python, linear regression and matplotlib

---
+++ life.py
@@ -0,0 +1,15 @@
+
+import math
+theanswer = 42 
+
+#def quote_marvin():
+    #print('i have calculated your chance of survival, but i don\'t think you will like it.')
+#quote_marvin()
+
+
+
+def square_root(num):
+    return math.sqrt(num)
+
+
+
